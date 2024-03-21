dc239abd1ab6cd833bf82f31c1f1ba8ae5080e29
Hasitha Ekanayake <150508298+h4sitha@users.noreply.github.com>
2024-03-21 20:26:56 +0530

    Update function to not run without a input value

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const resultDiv = document.querySelector('#result');
 
 button.addEventListener('click', () => {
     let string = input.value;
+    resultDiv.textContent = "";
+    if (!string) {
+        alert ("Please input a value");
+        return;
+    }
     let newString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
     let reversed = newString.split("").reverse().join("");
     if (reversed === newString) {
